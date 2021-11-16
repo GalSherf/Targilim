34972b20d70dc66abc11cbeca286f431ac7b3ae6
GalSherf <galsha97@gmail.com>
2021-11-16 16:10:53 +0200

    those are the latest changes

--- AOS _project/ShoppingCart.py
+++ AOS _project/ShoppingCart.py
@@ -10,3 +10,14 @@ class ShoppingCart:
 
     def shopping_text(self):
         return self.driver.find_element(By.CSS_SELECTOR, '[class="select  ng-binding"]').get_attribute("innerHTML")[1:]
+
+    def product_quantity(self):
+        quantity_list = self.driver.find_elements(By.CSS_SELECTOR, 'class="ng-binding"')
+        for i in quantity_list:
+            if i.get_attribute("title"):
+                quantity_list.remove(i)
+        print(len(quantity_list))
+
+    def product_color(self, number):
+        colors = self.driver.find_elements(By.CLASS_NAME, "productColor")
+        return colors[number].get_attribute("title")
