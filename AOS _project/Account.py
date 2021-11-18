1ee4013ccca109d64010087aae107423d2c9af69
GalSherf <galsha97@gmail.com>
2021-11-18 16:10:37 +0200

    those are the latest changes

--- AOS _project/Account.py
+++ AOS _project/Account.py
@@ -9,6 +9,7 @@ class Account:
         self.driver = driver
         self.wait = WebDriverWait(self.driver, 10)
 
+    # create account elements
     # return username input element
     def username(self):
         return self.driver.find_element(By.CSS_SELECTOR,'[name="usernameRegisterPage"]')
@@ -58,7 +59,7 @@ class Account:
     def state(self):
         return self.driver.find_element(By.CSS_SELECTOR, '[name="state_/_province_/_regionRegisterPage"]')
 
-    # return state input element
+    # return postal code input element
     def postal_code(self):
         return self.driver.find_element(By.CSS_SELECTOR, '[name="postal_codeRegisterPage"]')
 
@@ -66,49 +67,55 @@ class Account:
     def agree_conditions(self):
         return self.driver.find_element(By.CSS_SELECTOR, '[name="i_agree"]')
 
-    # return country element
+    # return register button
     def register_btn(self):
         return self.driver.find_element(By.ID, 'register_btnundefined')
 
+    # My Account page elements
+    # return edit account details button
     def edit_account_details(self):
         return self.driver.find_element(By.CSS_SELECTOR, '[href="#/accountDetails"]')
 
+    # return edit Preferred payment method button
     def edit_payment(self):
         return self.driver.find_element(By.CSS_SELECTOR, '[href="#/accountPaymentEdit"]')
 
+    # return delete account button element
+    def delete_account(self):
+        return self.driver.find_element(By.CSS_SELECTOR, '[class="deleteMainBtnContainer a-button ng-scope"]')
+
+    # return yes button that appear after clicking delete account
+    def confirm_delete(self):
+        self.delete_account().click()
+        return self.driver.find_element(By.CSS_SELECTOR, 'div[class="deletePopupBtn deleteRed"]')
+
+    # elements in login pop-up, appears after clicking user icon
+    # return username input element
     def popUp_username(self):
         return self.driver.find_element(By.CSS_SELECTOR, '[name="username"]')
 
+    # return password input element
     def popUp_password(self):
         return self.driver.find_element(By.CSS_SELECTOR, '[name="password"]')
 
+    # return sign in button element
     def signIn_button(self):
         return self.driver.find_element(By.CSS_SELECTOR, '[type="button"]')
 
+    # wait for sign in button to be clickable and clicks on it
     def click_sign_in(self):
         self.wait.until(EC.element_to_be_clickable((By.CSS_SELECTOR, '[type="button"]')))
         self.signIn_button().click()
 
+    # wait until X button (close the pop-up window) is clickable and returns it
     def x_button_pop_up(self):
         self.wait.until(EC.element_to_be_clickable((By.CLASS_NAME, "loginPopUpCloseBtn")))
         return self.driver.find_element(By.CLASS_NAME, "loginPopUpCloseBtn")
 
+    # return the user name next to user icon
     def loggedIn_username(self):
         return self.driver.find_element(By.CSS_SELECTOR, "#menuUserLink>span")
 
     # wait until the username text to be presented in element
     def wait_username_text(self):
-        self.wait.until_not(EC.text_to_be_present_in_element((By.CSS_SELECTOR, '#menuUserLink>[data-ng-show="userCookie.response"]'),self.loggedIn_username().text))
-
-    def user_menu(self):
-        return self.driver.find_elements(By.CSS_SELECTOR, "#menuUserLink>div>label")
-
-    def user_menu_options(self, number):
-        return self.user_menu()[number].get_attribute("innerHTML")
-
-    def delete_account(self):
-        return self.driver.find_element(By.CSS_SELECTOR, '[class="deleteMainBtnContainer a-button ng-scope"]')
-
-    def confirm_delete(self):
-        self.delete_account().click()
-        return self.driver.find_element(By.CSS_SELECTOR, 'div[class="deletePopupBtn deleteRed"]')
+        self.wait.until_not(EC.text_to_be_present_in_element((By.CSS_SELECTOR, '#menuUserLink>span'),self.loggedIn_username().text))
--- AOS _project/new.py
+++ AOS _project/new.py
@@ -13,6 +13,7 @@ from Product import *
 from Category import *
 from ShoppingCart import *
 from OrderPayment import *
+from Account import *
 
 service1 = Service(r"C:\selenium\chromedriver.exe")
 
@@ -27,24 +28,24 @@ product = Product(driver)
 category = Category(driver)
 shopping_cart = ShoppingCart(driver)
 order_payment = OrderPayment(driver)
+account = Account(driver)
 
+# homepage.laptops().click()
+# print(category.category_name())
+# category.click_on_product(-2)
+# product.add_to_cart().click()
+# homepage.shopping_cart().click()
+# shopping_cart.checkout_btn().click()
+# order_payment.username().send_keys("BasaLo")
+# order_payment.password().send_keys("Tester1")
+# order_payment.login_btn().click()
+# order_payment.next_btn().click()
+# order_payment.master_credit_radio().click()
+# order_payment.pay_now_btn().click()
 
-homepage.laptops().click()
-print(category.category_name())
-category.click_on_product(-2)
-product.add_to_cart().click()
-homepage.shopping_cart().click()
-shopping_cart.checkout_btn().click()
-order_payment.username().send_keys("BasaLo")
-order_payment.password().send_keys("Tester1")
-order_payment.login_btn().click()
-order_payment.next_btn().click()
-order_payment.master_credit_radio().click()
-order_payment.pay_now_btn().click()
+# print(shopping_cart.shopping_cart_empty().help())
 
-
-
-
+print(account.signIn_button.__doc__())
 
 sleep(4)
 driver.close()
